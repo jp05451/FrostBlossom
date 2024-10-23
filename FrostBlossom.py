783e2a78d395d8c9a0e8b03e110df7eb93b03f29
jp05451 <59051826+jp05451@users.noreply.github.com>
2024-10-23 17:14:34 +0800

    finish level 5 level 6 add drawFlow

--- FrostBlossom.py
+++ FrostBlossom.py
@@ -8,6 +8,7 @@ class vector:
         self.y = y
         self.angle = angle % 360
         self.length = length
+        self.color = (55, 155, 255)
 
     def getEnd(self):
         return self.x + self.length * math.cos(
@@ -44,12 +45,13 @@ class FrostBlossom:
         self.screen.title("FrostBlossom")
         self.screen.setup(width=1920, height=1080)
         self.screen.tracer(0)
+
         self.pen = turtle.Turtle()
         self.pen.speed(1)
+        self.pen.pencolor((55 / 255, 155 / 255, 255 / 255))
 
         self.level = level
-        # self.rootCursor = (x,y)
-        # self.length = length
+        self.centerCursor = CenterCursor
 
     def drawVector(self, v: vector):
         self.pen.penup()
@@ -65,21 +67,62 @@ class FrostBlossom:
         self.pen.goto(targetXY[0], targetXY[1])
         self.pen.penup()
 
+    def drawRing(self, centerCursor, width, radius):
+        color = self.pen.pencolor()
+
+        self.pen.penup()
+        self.pen.color((247 / 255, 202 / 255, 201 / 255))
+        self.pen.goto(centerCursor[0], centerCursor[1] - radius)
+        self.pen.width(width)
+        self.pen.pendown()
+        self.pen.circle(radius)
+        self.pen.penup()
+
+        self.pen.width(1)
+        self.pen.color(color)
+
+    def changeColor(self):
+        color = self.pen.pencolor()
+        r = min(int(color[0]) * 255 + 40, 255) / 255
+        g = min(int(color[1]) * 255 + 20, 255) / 255
+        b = int(color[2])
+        self.pen.pencolor((r, g, b))
+
+    def drawFlower(self, centerCursor, width, radius, petalNum=6):
+        tempV = vector(centerCursor[0], centerCursor[1], radius, 30)
+        self.pen.width(width)
+
+        for i in range(petalNum):
+            self.drawRing(tempV.getEnd(), width, radius / 2)
+            tempV.rotate(360 / petalNum)
+
+        self.pen.width(1)
+
     def drawTree(self, beginCursor, angle, length, level):
         if level == self.level:
             return
-        else:
+
+        if level <= 4:
             tempV = vector(beginCursor[0], beginCursor[1], length, angle)
             self.drawVector(tempV)
             endCursor = tempV.getEnd()
 
-            self.drawTree(endCursor, angle + 60, length * 0.5, level +1)
-            self.drawTree(endCursor, angle - 60, length * 0.5, level +1)
+            self.changeColor()
+            self.drawTree(endCursor, angle + 60, length * 0.5, level + 1)
+            self.drawTree(endCursor, angle - 60, length * 0.5, level + 1)
+
+        if level == 5:
+            self.drawRing(beginCursor, 5, 14)
+            self.drawTree(beginCursor, angle, length, level + 1)
+
+        if level == 6:
+            self.drawFlower(beginCursor, 3, 14)
+            self.drawTree(beginCursor, angle, length, level + 1)
 
 
 if __name__ == "__main__":
-    F = FrostBlossom(3)
+    F = FrostBlossom(7)
 
-    for i in range(0,360,60):
-        F.drawTree((0, 0), i, 200, 0)
+    F.drawTree(beginCursor=(0, 0), angle=90, length=200, level=0)
+
     turtle.done()
