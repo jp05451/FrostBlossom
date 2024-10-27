b7fe23c6fdda849445065849d3cc333949580acc
jp05451 <jp05451@yahoo.com.tw>
2024-10-27 21:40:14 +0800

    move vector to vector.py

--- FrostBlossom.py
+++ FrostBlossom.py
@@ -6,42 +6,6 @@ screenWith = 1920 * 0.7
 screenHigh = 1080 * 0.7
 
 
-class vector:
-    def __init__(self, x=0, y=0, length=1, angle=0):
-        self.x = x
-        self.y = y
-        self.angle = angle % 360
-        self.length = length
-        self.color = (55, 155, 255)
-
-    def getEnd(self):
-        return self.x + self.length * math.cos(
-            math.radians(self.angle)
-        ), self.y + self.length * math.sin(math.radians(self.angle))
-
-    def begin(self):
-        return self.x, self.y
-
-    def rotate(self, angle):
-        self.angle += angle
-        self.angle %= 360
-
-    def scale(self, scale):
-        self.length *= scale
-
-    def move(self, x, y):
-        self.x += x
-        self.y += y
-
-    def moveTo(self, x, y):
-        self.x = x
-        self.y = y
-
-    def setAngle(self, angle):
-        self.angle = angle
-        self.angle %= 360
-
-
 class FrostBlossom:
     def __init__(self, level=3, width=1920, high=1080):
         self.level = level
